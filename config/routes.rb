cf3d7cd7508176c3118c80ba8a6d73e0c822f8be
takehiro <takeishi19990912@gmail.com>
2021-02-13 15:15:47 +0900

    second

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   
 
   get 'home/top'
+  get "/"=>"home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
