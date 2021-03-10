9a2470bbef471165a73a75333ab31ad5086b9a7b
takehiro <takeishi19990912@gmail.com>
2021-03-10 17:40:48 +0900

    check

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,6 +9,8 @@ class LikesController < ApplicationController
   def destroy
     @task = Task.find_by(id: params[:post_id])
     @like=Like.find_by(user_id:@current_user.id,post_id:params[:post_id])
-　　 @like.destroy
+    if @like
+      @like.destroy
+    end
   end
 end
